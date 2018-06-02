c31d641c9255d1bc8e7b5726001f56a26117e881
Huang <huangp3@debian-9.3.0-template>
2018-06-01 20:22:19 -0700

    be able to run program with void *function(void* file)

--- searchfilep2.c
+++ searchfilep2.c
@@ -4,13 +4,46 @@
 #include <dirent.h> 
 #include <string.h> 
 #include <sys/time.h> 
+#include <pthread.h>
 #include <errno.h>
+#define threadNum 4
+#define max 80
 //takes a file/dir as argument, recurses, // prints name if empty dir or not a dir (leaves)
 
-void recur_file_search(char *file); 
+void *recur_file_search(void *file); 
+
+struct pathinfo{
+	char *path;
+	int threadid;
+};
+struct pathinfo *threadarg[threadNum];
+struct pathinfo *output;
+struct pathinfo *cp;
+
+
+pthread_t threads[threadNum];
+
 //share search term globally (rather than passing recursively) 
 const char *search_term; 
 int main(int argc, char **argv) {
+
+
+	for(int i = 0; i < threadNum; i++){
+	threadarg[i] = malloc(sizeof(struct pathinfo));
+	threadarg[i]->path = malloc(sizeof(char) * max);
+	}
+	output = malloc(sizeof(struct pathinfo));
+	output->path = malloc(sizeof(char) * max);
+	cp = malloc(sizeof(struct pathinfo));
+	cp->path = malloc(sizeof(char) * max);
+	
+
+
+
+//	cp->path = malloc(sizeof(char) * max);
+//	output = malloc(sizeof(struct pathinfo));
+//	output->path = malloc(sizeof(char) * max);
+
 	if(argc != 3)
 	{
 		printf("Usage: my_file_search <search_term> <dir>\n");
@@ -29,11 +62,21 @@ int main(int argc, char **argv) {
 		perror("opendir failed");
 		exit(1);
 	}
-	
+
 	//start timer for recursive search
 	struct timeval start, end;
 	gettimeofday(&start, NULL);
-	recur_file_search(argv[2]);
+	threadarg[0]->path = argv[2];
+	threadarg[0]->threadid = 0;
+//	printf("path %s\n",threadarg[0]->path);
+//	printf("threadid %d\n",threadarg[0]->threadid);
+//	printf("call function\n");
+
+
+	pthread_create(&threads[0],NULL, recur_file_search,(void*)threadarg[0]);
+//	recur_file_search((void*) &threadarg[0]);
+
+	pthread_join(threads[0],NULL);
 	gettimeofday(&end, NULL);
 	printf("Time: %ld\n", (end.tv_sec * 1000000 + end.tv_usec)
 			- (start.tv_sec * 1000000 + start.tv_usec));
@@ -49,9 +92,20 @@ int main(int argc, char **argv) {
 //Effects: prints the filename if the base case is reached *and* search_term
 // is found in the filename; otherwise, prints the directory name if the directory 
 // matches search_term. 
-void recur_file_search(char *file) {
-	//check if directory is actually a file
-	DIR *d = opendir(file);
+void *recur_file_search(void *file) {
+//check if directory is actually a file
+//	free(pathcp);
+	char *pathcp;
+//	pathcp = malloc(sizeof(char) * max);
+
+	cp = file;
+	pathcp = malloc(sizeof(char) * max);
+//	printf("pathcp:%s\n", pathcp);
+	pathcp = cp->path;
+//	printf("open:%s\n", pathcp);
+
+
+	DIR *d = opendir(pathcp);
 	//NULL means not a directory (or another, unlikely error)
 	if(d == NULL)
 	{
@@ -61,20 +115,21 @@ void recur_file_search(char *file) {
 		if(errno != ENOTDIR)
 		{
 			perror("Something weird happened!");
-			fprintf(stderr, "While looking at: %s\n", file);
+			fprintf(stderr, "While looking at: %s\n", pathcp);
 			exit(1);
 		}
 		//nothing weird happened, check if the file contains the search term
 		// and if so print the file to the screen (with full path)
-		if(strstr(file, search_term) != NULL)
-			printf("%s\n", file);
+		if(strstr(pathcp, search_term) != NULL)
+			printf("%s\n", pathcp);
 		//no need to close d (we can't, it is NULL!)
-		return;
+
+		return NULL;
 	}
 	//we have a directory, not a file, so check if its name
 	// matches the search term
-	if(strstr(file, search_term) != NULL)
-		printf("%s/\n", file);
+	if(strstr(pathcp, search_term) != NULL)
+		printf("%s/\n", pathcp);
 	//call recur_file_search for each file in d
 	//readdir "discovers" all the files in d, one by one and we
 	// recurse on those until we run out (readdir will return NULL)
@@ -89,21 +144,31 @@ void recur_file_search(char *file) {
 			// so here we append the discovered filename (cur_file->d_name)
 			// to the current path (file -- we know file is a directory at
 			// this point)
-			char *next_file_str = malloc(sizeof(char) * \
-					strlen(cur_file->d_name) + \
-					strlen(file) + 2);
-			strncpy(next_file_str, file, strlen(file));
-			strncpy(next_file_str + strlen(file), \
+//			printf("alloc memory\n");
+			char *next_file_str = malloc(sizeof(char) * 
+					strlen(cur_file->d_name) + 
+					strlen(pathcp) + 2);
+//			printf("copy old string: %s\n", pathcp);
+
+			strncpy(next_file_str,pathcp, strlen(pathcp));
+
+			strncpy(next_file_str + strlen(pathcp), 
 					"/", 1);
-			strncpy(next_file_str + strlen(file) + 1, \
-					cur_file->d_name, \
+			strncpy(next_file_str + strlen(pathcp) + 1, 
+					cur_file->d_name, 
 					strlen(cur_file->d_name) + 1);
+
 			//recurse on the file
-			recur_file_search(next_file_str);
+//			printf("next file name: %s\n", next_file_str);
+//			printf("==============================================\n");
+			output->path = next_file_str;
+//			printf("output path: %s\n", output->path);
+			recur_file_search((void*)output);
 			//free the dynamically-allocated string
 			free(next_file_str);
 		}
 	}
 	//close the directory, or we will have too many files opened (bad times)
+//	free(pathcp);
 	closedir(d);
 }
